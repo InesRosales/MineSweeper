d89ab33ca381aa7e853d2a673b70d0ab8dce2cea
Ines Rosales <ines.velez@gmail.com>
2017-07-23 21:44:21 -0500

    Expansion
    The Board now clears empty elements when clicked on a empty cell

--- app/src/main/java/com/meetkokomo/www/minesweeper/MineSweeper.kt
+++ app/src/main/java/com/meetkokomo/www/minesweeper/MineSweeper.kt
@@ -2,7 +2,9 @@ package com.meetkokomo.www.minesweeper
 
 import android.util.Log
 import java.util.Random
-import java.util.concurrent.ConcurrentLinkedDeque
+import java.util.Stack
+
+
 
 /**
  * Created by morningstar on 7/22/17.
@@ -10,7 +12,7 @@ import java.util.concurrent.ConcurrentLinkedDeque
 
 class MineSweeper {
 
-    val MAX_MINES : Int = 10
+    val MAX_MINES : Int = 2
     val ROW : Int = 8
     val COL : Int = 8
 
@@ -46,7 +48,9 @@ class Board (numRow : Int, numCol : Int) {
     val ROW : Int
     val COL : Int
     val MINE_CELL = 9
+    val EMPTY_CELL = 10
     val MINE_CELL_PRESSED = -1
+    val EMPTY_CELL_PRESSED = 0
 
     var data : IntArray
 
@@ -84,14 +88,13 @@ class Board (numRow : Int, numCol : Int) {
 
     }
 
-    fun valid (col : Int, row : Int) : Boolean {
+    fun isValid(col : Int, row : Int) : Boolean {
         return (col < COL && col >= 0 && row < ROW && row >= 0)
     }
 
     fun updateNeighbor (col : Int, row : Int) {
 
-        val valid = valid(col, row)
-        if(valid == true) {
+        if(isValid(col, row)) {
             val element: Int = getElementAt(col, row)
 
             if (element != MINE_CELL) {
@@ -104,9 +107,71 @@ class Board (numRow : Int, numCol : Int) {
     fun click (col : Int, row : Int){
         val element = getElementAt(col, row)
 
-        if(element >= MINE_CELL){
-            setElement(element-10, col, row)
+        if(isMine(element)){
+            Log.d("Board", "GAME OVER")
         }
+        else if (element >= MINE_CELL){
+            expansion(col, row)
+        }
+    }
+
+    fun expansion (col : Int, row: Int){
+
+        var stack = Stack<Int> ()
+
+        stack.push((row * COL) +  col)
+
+        while (!stack.empty()){
+            var temp = stack.pop()
+
+            val col = temp % COL
+            var row = temp / COL
+
+            if(data[temp] == 10){
+
+                data[temp] = 0
+
+                if (isValid(col-1, row-1)) {
+                    stack.push(((row - 1) * COL) + (col - 1))
+                }
+
+                if (isValid(col, row-1)) {
+                    stack.push(((row - 1) * COL) + col)
+                }
+                if (isValid(col+1,row-1)) {
+                    stack.push(((row - 1) * COL) + (col + 1))
+                }
+
+                if (isValid (col-1, row)) {
+                    stack.push((row * COL) + (col - 1))
+                }
+
+                if (isValid(col+1, row)) {
+                    stack.push((row * COL) + (col + 1))
+                }
+
+                if (isValid(col - 1, row + 1)){
+                    stack.push(((row + 1) * COL) + (col - 1))
+                }
+
+                if (isValid(col, row + 1)) {
+                    stack.push(((row + 1) * COL) + col)
+                }
+
+                if (isValid(col + 1, row + 1)) {
+                    stack.push(((row + 1) * COL) + (col + 1))
+                }
+
+            }
+            else if (data[temp] > 10){
+                data[temp] -= 10
+            }
+        }
+
+    }
+
+    fun isMine(element : Int) : Boolean {
+        return (element == MINE_CELL)
     }
 
     fun getElementAt (col : Int, row: Int) : Int {
@@ -114,36 +179,30 @@ class Board (numRow : Int, numCol : Int) {
     }
 
     fun setElement (value: Int, col: Int, row: Int){
-        if( valid(col, row)){
+        if( isValid(col, row)){
             data [(row * COL) +  col] = value;
         }
     }
 
     fun printBoard() {
-        var nRow : String = ""
-        var cCount : Int = 0
-
-        for (element in data){
-            if(element >= MINE_CELL){
-                nRow += " 0 "
+        var nRow: String = ""
+        for ((index, value) in data.withIndex()) {
+            if (index >= COL) {
+                if (index % COL == 0)
+                    nRow += "\n"
             }
-            else
-            {
-                if(element == MINE_CELL_PRESSED){
-                    nRow += " X "
-                }
-                else{
-                    nRow += " " + element + " "
-                }
+            if(value >= MINE_CELL) {
+                nRow += "  0 " //TODO change here to 0
             }
+            else {
+                if (value == MINE_CELL_PRESSED)
+                    nRow += "  X "
+                else if (value == EMPTY_CELL_PRESSED)
+                    nRow += "  - "
+                else
+                    nRow += "  " + value + " "
 
-            cCount++
-
-            if(cCount == COL){
-                nRow += "\n"
-                cCount = 0
             }
-
         }
         Log.d("Board", nRow)
     }
